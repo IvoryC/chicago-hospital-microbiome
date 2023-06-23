759a30b985ef64da5cdc066e4bfeea1ebe486b3a
Ivory Blakley <IvoryEC@gmail.com>
2023-06-23 13:18:15 -0400

    Assign taxanomy

--- 06_assign_ASV_taxonomy/dada2_assign_taxonomy.R
+++ 06_assign_ASV_taxonomy/dada2_assign_taxonomy.R
@@ -8,17 +8,20 @@
 # BiocManager::install("dada2", version = "3.17")
 library(dada2); packageVersion("dada2")
 
+#### direct output ####
+
+outDir = "output"
+suppressWarnings(dir.create(outDir, recursive = T))
+
 #### find ASV file ####
 
 # asvFile = file.path(dir("..", pattern="DADA2$", full.names = T),
 #                     "output", "asv.txt")
 inputModule = dir("..", pattern="DADA2$", full.names = T)
-inputDir = dir(inputModule, pattern="output", full.names = T)
-asvFile = dir(inputDir, pattern="asv-id-seq", full.names = T, recursive = T)
+asvFile = dir(inputModule, pattern="asv-id-sequence.txt", full.names = T, recursive = T)
 
 asv.id.table = read.delim2(asvFile)
 message("Read in ", nrow(asv.id.table), " ASVs.")
-# Read in 40026 ASVs.
 
 # assign taxa
 taxa <- assignTaxonomy(seqs=asv.id.table$ASV, "~/tax/silva_nr99_v138.1_train_set.fa.gz", multithread=TRUE)
@@ -31,9 +34,14 @@ taxaS <- addSpecies(taxa, "~/tax/silva_species_assignment_v138.1.fa.gz")
 # or (same solution) here: 
 # in short, add line "R_MAX_VSIZE=14Gb " to file ~/.Renviron
 
-taxa$ASV = row.names(taxa)
+taxaS2 = data.frame(taxaS)
+taxaS2$ASV = row.names(taxaS)
 
-asvTaxa = merge(asv.id.table, taxa, by="ASV")
+asvTaxa = merge(asv.id.table, taxaS2, by="ASV")
+
+# match the original order
+row.names(asvTaxa) = asvTaxa$asvID
+asvTaxa = asvTaxa[asv.id.table$asvID,]
 
 # Save taxa info.
 write.table(asvTaxa, 
